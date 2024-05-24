5cf85ae819d29ce9b0fb0e7ba1cb081c3fa56d55
Edgar Costa <edgarc@mit.edu>
2024-05-24 09:44:04 -0400

    adding None

--- None.m
+++ None.m
@@ -1,6 +1,8 @@
+freeze;
+
 declare type None;
 
-instrinsic IsNone(x::Any) -> BolElt
-{ Checks if x is None }
+intrinsic IsNone(x::Any) -> BoolElt
+  { Checks if x is None }
   return x cmpeq None;
-end instrinsic;
+end intrinsic;
