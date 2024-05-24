bc5b9b9c49ac0cf613f4aa8832d52c567f807edc
Edgar Costa <edgarc@mit.edu>
2024-05-24 09:43:44 -0400

    enabling Parameters

--- Call.m
+++ Call.m
@@ -1,20 +1,21 @@
-function _Call(f, input, nvals, optional) // to allow f to be an UserProgram or Intrinsic
+// to allow f to be an UserProgram or Intrinsic
+// FIXME: this should be supported in kernel...
+function _Call(f, input, parameters, number_of_results)
   tmp := f;
   input_string := Join([Sprintf("input[%o]", i) : i->_ in input], ", ");
-  if #optional ge 1 then
+  if #parameters ge 1 then
     input_string cat:= ": ";
-    input_string cat:= Join([Sprintf("%o:=optional[%o, 2]", t[1], i) : i->t in optional], ", ");
+    input_string cat:= Join([Sprintf("%o:=parameters[%o, 2]", t[1], i) : i->t in parameters], ", ");
   end if;
-  output_string := Join([Sprintf("output[%o]", i) : i in [1..nvals]], ", ");
+  output_string := Join([Sprintf("__output_%o", i) : i in [1..number_of_results]], ", ");
+  output_parser_string := Join([Sprintf("assigned __output_%o select __output_%o else None", i, i) : i in [1..number_of_results]], ", ");
   // The string that is used in the eval expression can refer to any variable that is in scope during the evaluation of the eval expression.
   // However, it is not possible for the expression to modify any of these variables.
   call_string := Sprintf("
-  output := [* None : _ in [1..nvals] *];
   %o := tmp(%o);
-  return output;", output_string, input_string);
+  return [* %o *];", output_string, input_string, output_parser_string);
   try
     output := eval call_string;
-    output := [* elt: elt in output | output cmpne None *];
     success_call := true;
   catch e
     print e;
@@ -24,12 +25,12 @@ function _Call(f, input, nvals, optional) // to allow f to be an UserProgram or 
   return success_call, output;
 end function;
 
-intrinsic Call(f::UserProgram, input::Tup, nvals::RngIntElt : optional:=<> ) -> BoolElt, List
-  { Call f on the input and return nvals }
-  return _Call(f, input, nvals, optional);
+intrinsic Call(f::UserProgram, input::Tup, number_of_results::RngIntElt : Parameters:=<> ) -> BoolElt, List
+  { Call f on the input expecting return_nvals }
+  return _Call(f, input, Parameters, number_of_results);
 end intrinsic;
 
-intrinsic Call(f::Intrinsic, input::Tup, nvals::RngIntElt : optional:=<> ) -> BoolElt, List
+intrinsic Call(f::Intrinsic, input::Tup, number_of_results::RngIntElt : Parameters:=<> ) -> BoolElt, List
   { " } //"
-  return _Call(f, input, nvals, optional);
+  return _Call(f, input, Parameters, number_of_results);
 end intrinsic;
