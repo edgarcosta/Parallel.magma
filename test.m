d48de3fb4651a0569ab45eecffc86e0d450bb79f
Edgar Costa <edgarc@mit.edu>
2024-06-02 17:44:16 -0400

    ParallelCall

--- test.m
+++ test.m
@@ -1,7 +1,9 @@
 AttachSpec("spec");
-ns := [NextPrime(2^64-2^32*i)*NextPrime(2^64+2^32*i) : i in [1..10]];
-time p := function()
-  return ParallelCall(10, Factorization, [<n> : n in ns], 1);
-end function();
-time np := [Factorization(n) : n in ns];
-[p[i, 2, 1] eq elt : i->elt in np];
+// we wrap the whole in a procedure to make sure
+// that is the file is read and closed before any forking
+procedure ()
+  ns := [NextPrime(2^64-2^32*i)*NextPrime(2^64+2^32*i) : i in [1..10]];
+  time p := ParallelCall(10, Factorization, [<n> : n in ns], 1);
+  time np := [Factorization(n) : n in ns];
+  [p[i, 2, 1] eq elt : i->elt in np];
+end procedure();
