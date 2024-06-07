8f6710a42fb630f6f45458e4851c336dcf00c72f
Edgar Costa <edgarc@mit.edu>
2024-06-07 14:31:39 -0400

    Update example_ParallelCall.m

--- example_ParallelCall.m
+++ example_ParallelCall.m
@@ -2,7 +2,7 @@
 // that is the file is read and closed before any forking
 jobs := StringToInteger(jobs);
 procedure ()
-  ns := [NextPrime(2^64-2^32*i)*NextPrime(2^64+2^32*i) : i in [1..10]];
+  ns := [NextPrime(2^64-2^32*i)*NextPrime(2^64+2^32*i) : i in [1..jobs]];
   time p := ParallelCall(jobs, Factorization, [<n> : n in ns], 1);
   time np := [Factorization(n) : n in ns];
   [p[i, 2, 1] eq elt : i->elt in np];
